c79a811bfe7697ab34233a83da978068deb6c2f4
SoumyaSJha <soumyajha126@gmail.com>
2023-05-15 23:16:04 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,81 @@
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4" >
+  <div style="background-color: #343a40; color : white; font-size: 12px;">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Railways.Live</h6>
+        <p>This is an initial level website aimed to be useful for booking tickets from anywhere in the world.
+          This is a part of project for the course of <b>DBMS</b></p><hr>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+        <p>
+          <a href="register.php">Register</a>
+        </p>
+        <p>
+          <a href="pnr.php">Inquiry</a>
+        </p>
+        <p>
+          <a href="train_stas.php">Train Schedule</a>
+        </p>
+        
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+   
+      <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2020 Copyright:
+          <a href="http://www.railways.live">
+            <strong> railways.live</strong>
+          </a>
+        </p>
+
+      </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+</div>
+
+</footer>
+<!-- Footer -->
+
