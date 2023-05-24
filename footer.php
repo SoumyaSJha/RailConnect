4b7f97e46baa88ac1c1933337139691a67f24837
SoumyaSJha <soumyajha126@gmail.com>
2023-05-25 00:36:57 +0530

    revised errors

--- footer.php
+++ footer.php
@@ -1,27 +1,18 @@
 <footer class="page-footer font-small mdb-color pt-4" >
   <div style="background-color: #343a40; color : white; font-size: 16px;">
 
-  <!-- Footer Links -->
+
   <div class="container text-center text-md-left">
 
-    <!-- Footer links -->
+ 
     <div class="row text-center text-md-left mt-4 pb-4">
 
-      <!-- Grid column -->
+  
       <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-4">
         <h6 class="text-uppercase mb-4 font-weight-bold" style="font-size:20px ;">Railways Connect: Your Gateway to Unforgettable Journeys</h6>
 
         <p >At Railways Connect, we believe that every journey holds the promise of adventure, discovery, and unforgettable memories. With our commitment to excellence, we strive to provide you with a seamless and delightful travel experience from start to finish.</p>
       </div>
-      <!-- Grid column -->
-
-
-      <!-- Grid column -->
-
-      <!-- Grid column -->
-
-
-      <!-- Grid column -->
       <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
   <h6 class="text-uppercase mb-4 font-weight-bold" style="font-size:20px;">Useful links</h6>
   <p>
@@ -35,29 +26,12 @@
   </p>
 </div>
 
-
-      <!-- Grid column -->
-
-      <!-- Grid column -->
-   
-      <!-- Grid column -->
-
     </div>
-    <!-- Footer links -->
-
     <br>
     <br>
-
-    <!-- Grid row -->
-  
-      <!-- Grid column -->
-
     </div>
-    <!-- Grid row -->
-
   </div>
-  <!-- Footer Links -->
 </div>
 
 </footer>
-<!-- Footer -->
+
